605e13a7325ff21cef5755922628753c6eef5c40
Misha McFeat <misha.mcfeat@gmail.com>
2023-09-09 01:46:22 +0100

    added all ORM tables

--- app/models/database.py
+++ app/models/database.py
@@ -1,15 +1,72 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import create_engine, Column, Integer, Float, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+import datetime
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)  # ORM: This is our primary key, auto-incremented by the database
-    username = Column(String(150), unique=True, index=True)  # ORM: A unique username column
-    email = Column(String(150), unique=True, index=True)     # ORM: A unique email column
-    password = Column(String(200))                           # ORM: Password column
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(150), unique=True, index=True)
+    email = Column(String(150), unique=True, index=True)
+    password = Column(String(200))
     address = Column(String(300), index=True)
     phone_number = Column(String(20), unique=True, index=True)
-    # ... Add other fields if needed, like address, phone_number etc.
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(150), unique=True, index=True)
+    address = Column(String(300), index=True)
+    # Additional restaurant-related fields can be added here
+
+    items = relationship("RestaurantItem", backref="restaurant")
+
+
+class RestaurantItem(Base):
+    __tablename__ = 'restaurant_items'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(150), index=True)
+    description = Column(String(300))
+    price = Column(Float)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+
+
+class Order(Base):
+    __tablename__ = 'orders'
+
+    order_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'), nullable=False)
+    total_amount = Column(Float, nullable=False)
+    status = Column(String, nullable=False) # Can use Enum for predefined statuses
+    delivery_address = Column(String, nullable=False)
+    time_placed = Column(DateTime, default=datetime.datetime.utcnow)
+
+    order_items = relationship("OrderItem", backref="order")
+
+
+class OrderItem(Base):
+    __tablename__ = 'order_items'
+
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey('orders.order_id'), nullable=False)
+    restaurant_item_id = Column(Integer, ForeignKey('restaurant_items.id'), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    price_at_time_of_order = Column(Float, nullable=False)
+
+
+class Payment(Base):
+    __tablename__ = 'payments'
+
+    transaction_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    order_id = Column(Integer, ForeignKey('orders.order_id'), nullable=True) # Nullable because not all payments are linked to orders
+    amount = Column(Float, nullable=False)
+    status = Column(String, nullable=False) # Can use Enum like "SUCCESS", "FAILED", "PENDING", etc.
+    transaction_time = Column(DateTime, default=datetime.datetime.utcnow)
--- app/schemas/restaurant.py
+++ app/schemas/restaurant.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List, Optional
 
 class MenuItemBase(BaseModel):
@@ -8,17 +8,18 @@ class MenuItemBase(BaseModel):
 class MenuItemCreate(MenuItemBase):
     pass
 
-class MenuItemUpdate(MenuItemBase):
-    pass
+class MenuItemUpdate(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
 
 class MenuItem(MenuItemBase):
     item_id: int
 
 class Restaurant(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
     restaurant_id: int
     name: str
     address: str
     menu: List[MenuItem]
 
-    class Config:
-        orm_mode = True
